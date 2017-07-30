0000170c5359f74eb90cfc0b10fb5c9879188c48
Piet Bijl <piet@bureaupieper.nl>
2017-07-30 10:31:05 +0200

    POC profiler data

---
+++ src/Processor/Symfony/RequestProfiler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Appeltaert\PAM\Processor\Symfony;
+
+
+use Appeltaert\PAM\Processor\ProcessorInterface;
+use function Appeltaert\PAM\callGetters;
+
+class RequestProfiler implements ProcessorInterface
+{
+    public function getIdentifier()
+    {
+        return 'Request profile';
+    }
+
+    public function accepts($context)
+    {
+        $expected = 'Symfony\Component\HttpKernel\DataCollector\RequestDataCollector';
+        return is_object($context) && $context instanceof $expected;
+    }
+
+    public function normalize(array $collection, $context, $verbose)
+    {
+        $return = [];
+
+        $return = array_merge($return, callGetters($context, [
+            'Format',
+            'StatusText',
+            'Route',
+            'StatusCode',
+            'ContentType',
+            'PathInfo'
+        ]));
+
+        return $return;
+    }
+
+}
